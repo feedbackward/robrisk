0d2c682c31e37047c274ab1524000de8fc5e906c
mjh <matthew-h@ar.sanken.osaka-u.ac.jp>
2021-02-18 11:11:22 +0900

    Added RGD-M.

--- robrisk/learn_run.sh
+++ robrisk/learn_run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-ALGO="SGD"
+ALGO="RGD-M"
 ALPHA="0.05"
-BATCH="1"
-DATA="ds_normal"
+BATCH="0"
+DATA="ds_lognormal"
 LOSS="quadratic"
 MODEL="linreg"
-EPOCHS="30"
+EPOCHS="40"
 TASK="default"
 TRIALS="100"
-STEP="1.0"
+STEP="0.1"
 
 python "learn_driver.py" --algo="$ALGO" --alpha="$ALPHA" --batch-size="$BATCH" --data="$DATA" --loss="$LOSS" --model="$MODEL" --num-epochs="$EPOCHS" --num-trials="$TRIALS" --step-size="$STEP" --task-name="$TASK"
-
--- robrisk/setup_algos.py
+++ robrisk/setup_algos.py
@@ -5,9 +5,22 @@ import numpy as np
 
 ## Internal modules.
 from mml.algos.gd import GD_ERM
+from mml.algos.rgd import RGD_Mest
+from mml.utils.mest import est_loc_fixedpt, inf_gudermann, est_scale_chi_fixedpt, chi_geman_quad
 
 
 ###############################################################################
+
+
+## Detailed setup for algorithms.
+
+_inf_fn = inf_gudermann # influence function.
+_est_loc = lambda X, s, thres, iters: est_loc_fixedpt(X=X, s=s,
+                                                      inf_fn=_inf_fn,
+                                                      thres=thres,
+                                                      iters=iters)
+_chi_fn = chi_geman_quad # chi function.
+_est_scale = lambda X: est_scale_chi_fixedpt(X=X, chi_fn=_chi_fn)
 
 
 ## Simple parser for algorithm objects.
@@ -18,6 +31,15 @@ def get_algo(name, model, loss, **kwargs):
         return GD_ERM(step_coef=kwargs["step_size"],
                       model=model,
                       loss=loss)
+    elif name == "RGD-M":
+        return RGD_Mest(est_loc=_est_loc,
+                        est_scale=_est_scale,
+                        delta=0.01,
+                        mest_thres=1e-03,
+                        mest_iters=50,
+                        step_coef=kwargs["step_size"],
+                        model=model,
+                        loss=loss)
     else:
         return None
                       
