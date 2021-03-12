8d3da28d807d4d47a43e29de5fce8363cbca3734
mjh <matthew-h@ar.sanken.osaka-u.ac.jp>
2021-03-12 16:14:59 +0900

    Default to settings for real-world data.

--- robrisk/learn_rb_run.sh
+++ robrisk/learn_rb_run.sh
@@ -11,7 +11,7 @@ MODEL="linreg_multi"
 EPOCHS="30"
 PROCS="10"
 TASK="default"
-TRIALS="10"
+TRIALS="50"
 STEP="0.01"
 
 python "learn_rb_driver.py" --algo-ancillary="$ALGO_ANCILLARY" --algo-main="$ALGO_MAIN" --alpha="$ALPHA" --batch-size="$BATCH" --data="$DATA" --entropy="$ENTROPY" --loss="$LOSS" --model="$MODEL" --num-epochs="$EPOCHS" --num-processes="$PROCS" --num-trials="$TRIALS" --step-size="$STEP" --task-name="$TASK"
