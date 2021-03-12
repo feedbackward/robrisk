824f88a0b4270ab7f5fb7fd1eaf6d66fe17d1e80
mjh <matthew-h@ar.sanken.osaka-u.ac.jp>
2021-03-12 16:44:21 +0900

    Default real-data experiment settings.

--- robrisk/learn_run.sh
+++ robrisk/learn_run.sh
@@ -10,7 +10,7 @@ LOSS="logistic"
 MODEL="linreg_multi"
 EPOCHS="30"
 TASK="default"
-TRIALS="50"
+TRIALS="20"
 STEP="0.1"
 
 python "learn_driver.py" --algo-ancillary="$ALGO_ANCILLARY" --algo-main="$ALGO_MAIN" --alpha="$ALPHA" --batch-size="$BATCH" --data="$DATA" --entropy="$ENTROPY" --loss="$LOSS" --model="$MODEL" --num-epochs="$EPOCHS" --num-trials="$TRIALS" --step-size="$STEP" --task-name="$TASK"
